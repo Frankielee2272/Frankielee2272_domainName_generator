ac422be7dfad11b5b0fd04531d0261b0d4308b3e
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-02-22 01:38:38 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Write your code here
+  console.log("Hello Frank from the console!");
+
+  // Create arrays for domain name generation
+  let pronoun = ["the", "our", "him", "her", "them", "she", "he"];
+  let adj = ["great", "big", "small", "fancy", "pretty"];
+  let noun = ["jogger", "racoon", "swimmer", "driver", "blacksmith", "rider"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".net");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".us");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".io");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".etc");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".net");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".us");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".io");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".org");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".net");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".us");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".io");
+        console.log(pronoun[i] + adj[j] + noun[k] + ".gov");
+      }
+    }
+  }
 };
